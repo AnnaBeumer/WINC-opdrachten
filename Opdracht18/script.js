103b783cc892cd0113ce39faeef7089a099cde50
Anna Beumer <44589145+AnnaBeumer@users.noreply.github.com>
2020-11-26 12:00:55 +0100

    Add files via upload

---
+++ Opdracht18/script.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy studenten");
+
+// Opdracht 18 deel 2
+// Punt 1
+// Dit is een grote som
+/*let sum1 = 1230941 * 1823794;
+console.log(sum1);
+// Punt 2
+let sum2 = 637263 / 54;
+console.log(sum2);
+// Punt 3 
+// Zie eerste comment punt 1
