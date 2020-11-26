c5c2baa5851354def9349971d87c27c033f669c8
Anna Beumer <44589145+AnnaBeumer@users.noreply.github.com>
2020-11-26 14:20:17 +0100

    Add files via upload

---
+++ Opdracht19/script.js
@@ -0,0 +1,21 @@
+const age = 17;
+const isFemale = false;
+const driverStatus = "Henk";
+
+if (age > 18) {
+  console.log("Leeftijd is groter dan 18");
+} else {
+  console.log("Leeftijd is kleiner dan 18");
+}
+
+if (isFemale) {
+  console.log("Bezoeker is een vrouw");
+} else {
+  console.log("Bezoeker is geen vrouw");
+}
+
+if (driverStatus == "Bob") {
+  console.log("Driver status Bob, mag rijden");
+} else{
+    console.log("Driver status, mag niet rijden");
+}
