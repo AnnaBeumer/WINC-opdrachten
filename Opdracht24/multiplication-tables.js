da45f9b650066f0cdb224c5ef5b52e473c974c6b
Anna Beumer <44589145+AnnaBeumer@users.noreply.github.com>
2020-12-01 14:22:17 +0100

    Add files via upload

---
+++ Opdracht24/multiplication-tables.js
@@ -0,0 +1,9 @@
+// let tableNumber = 2;
+
+for (i = 0; i <= 10; i++) {
+    for (tableNumber =0; tableNumber <= 10; tableNumber++){
+        let result = i * tableNumber;
+  console.log(tableNumber + " * " + i + " = " + result);
+    }
+  
+}
