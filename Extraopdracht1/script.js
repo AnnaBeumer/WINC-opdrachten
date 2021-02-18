5acbb6bb7eb17380c519ba124847f05f017a3d31
Anna Beumer <anna.m.beumer@gmail.com>
2021-02-18 14:43:22 +0100

    added ExtraOpdracht1

---
+++ Extraopdracht1/script.js
@@ -0,0 +1,14 @@
+const makePizza = (topping) => {
+  console.log("This pizza contains the following: " + topping);
+};
+
+makePizza("tomatosauce, cheese, union.");
+makePizza("tomatosauce, mozzarella, salami.");
+
+const makeSushi = (topping) => {
+  console.log("This sushi contains:" + topping);
+};
+
+makeSushi("rice, salmon.");
+makeSushi("rice, tuna");
+makeSushi("rice, advocado");
